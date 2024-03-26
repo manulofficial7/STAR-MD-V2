f95eb93008fcbf9a6e9270442607d3f55a5d5506
©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️ <157870475+manulofficial7@users.noreply.github.com>
2024-03-26 22:02:15 +0530

    Update Main-DEVS.js

--- plugins/Main-DEVS.js
+++ plugins/Main-DEVS.js
@@ -2,16 +2,16 @@ var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "𝐇𝐞𝐲" }, "message":{ "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
   let pp = './Assets/STAR-V2.jpg'
 
-const cat = `*_STAR-V2_*
+const cat = `*_MANU-V2_*
 
 
-*_—🔰 𝘚𝘛𝘈𝘙-𝘔𝘋  𝐃𝐄𝐕 𝟏 wa.me/+2347045035241 %i'm here to help%_*
+*_—🔰 𝘔𝘈𝘕𝘜-𝘔𝘋  𝐃𝐄𝐕 𝟏 wa.me/+94760069363 %i'm here to help%_*
 
-*_—🔰 𝘚𝘛𝘈𝘙-𝘔𝘋  𝐃𝐄𝐕 2 wa.me/+2347045038687_*
+*_—🔰 𝘔𝘈𝘕𝘜-𝘔𝘋  𝐃𝐄𝐕 2 wa.me/+94760069363_*
 *---------------------*
 
 
-*_ᴛʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀ_*
+*_©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️_*
 *${developer}*`
 
 await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak)
